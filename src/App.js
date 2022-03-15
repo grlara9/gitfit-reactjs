813ef9419bdfafe27e6363fbe6dd025187e40262
Guillermo <grlara9@gmail.com>
2022-03-14 23:46:27 -0500

    apply css styles to header and navigation and overall page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navigation from './ui/Navigation';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+
       <Navigation />
+      </Router>
     </div>
   );
 }
