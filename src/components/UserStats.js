0837cd07aa13b6560995a8f2c462c8ed3fdbd49d
Guillermo <grlara9@gmail.com>
2022-03-22 00:40:41 -0500

    trying to include icons for expanding info

--- src/components/UserStats.js
+++ src/components/UserStats.js
@@ -8,27 +8,27 @@ const UserStats = () =>{
             <Card.Body>
                 <Card.Title>Card Title</Card.Title>
                 <p>
-            <strong>ID</strong>:</p>
+            <strong>ID</strong>: 12334</p>
           
             <p>
-              <strong>Name</strong>:</p>
+              <strong>Name</strong>: Guillermo</p>
            
               <p>
-                <strong>Age:</strong> </p>
+                <strong>Age:</strong> 23</p>
               <p>
-                <strong>Height</strong>:  
+                <strong>Height</strong>: 123 
               </p>
               <p>
-                <strong>Weight</strong>:  lbs
+                <strong>Weight</strong>:  145lbs
               </p>
               <p>
-                <strong>Goal</strong>: 
+                <strong>Goal</strong>: gain wait
               </p>
               <p>
-                <strong>Physical Activity</strong>: 
+                <strong>Physical Activity</strong>: light
               </p>
               <p>
-                <strong>Sex</strong>: 
+                <strong>Sex</strong>: male <i class="material-icons">expand_more</i>
               </p>
                 <Card.Link href="#">Another Link</Card.Link>
             </Card.Body>
