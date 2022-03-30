0f841fe2080317aa7c8f06416e31d458f1602dfe
Guillermo <grlara9@gmail.com>
2022-03-29 20:28:19 -0500

    finish with beginner and intermediate components

--- src/components/BeginnerPlan.js
+++ src/components/BeginnerPlan.js
@@ -3,7 +3,37 @@ import React from "react";
 const Beginner = () => {
     return(
         <div className="beginner">
-            <h1>hola</h1>
+               <h3>Monday</h3>
+    <ul>
+        <li>Dumbbell Squats: 3 sets of 5-6 reps</li>
+        <li>Bodyweight Push Ups: 3 sets of 10 reps</li>
+        <li>Dumbbell Row: 3 sets of 10 reps</li>
+        <li>Walking Lunges (no added weight): 3 sets of 10 reps each leg </li>
+        <li>Crunches: 3 sets of 10-12 reps</li>
+    </ul>
+​
+    
+​
+    <h3>Wednesday</h3>
+    <ul>
+        <li>Lat Pull Down: 3 sets of 10 reps</li>
+        <li>Leg Press Machine: 3 sets of 10 reps</li>
+        <li>Dumbell Bench Press: 3 sets of 10 reps</li>
+        <li>Burpees: 3 sets of 10 reps</li>
+        <li>Plank: Hold for 30 seconds</li>
+    </ul>
+​
+    
+​
+    <h3>Friday</h3>
+    <ul>
+        <li>Barbell Bench Press: 3 sets of 8 reps</li>
+        <li>Barbell Squats 3 sets of 12 reps</li>
+        <li>Seated Cable Row: 3 sets of 10 reps</li>
+        <li>Seated Dumbbell Shoulder Press: 3 sets of 10 reps</li>
+        <li>Reverse Crunches: 3 sets of 12 reps</li>
+        <li>Sit Ups: 3 sets of 8-10 reps</li>
+    </ul>
         </div>
     )
 }
--- src/components/IntermediatePlan.js
+++ src/components/IntermediatePlan.js
@@ -3,7 +3,57 @@ import React from "react";
 const Intermediate = () =>{
     return(
         <div>
-            <h1>hola</h1>
+             <h3>Monday (Legs/Abs)</h3>
+    <ul>
+        <li>Barbell Back Squats: 4 sets of 8 reps</li>
+        <li>Leg Press Machine: 4 sets of 10 reps</li>
+        <li>Bodyweight Glute Bridges: 4 sets of 12 reps</li>
+        <li>Stiff Legged Deadlifts: 3 sets of 10 reps </li>
+        <li>Reverse Lunges: 3 sets of 10 reps each leg</li>
+        <li>Sit Ups: 4 sets of 12 reps</li>
+        <li>Plank: Hold for 45 seconds</li>
+    </ul>
+​
+   
+​
+    <h3>Wednesday (Chest/Shoulders/Triceps)</h3>
+    <ul>
+        <li>Barbell Bench Press: 4 sets of 10 reps</li>
+        <li>Incline Barbell Bench Press: 4 sets of 10 reps</li>
+        <li>Standing Overhead Press: 3 sets of 10 reps</li>
+        <li>Side Lateral Raises: 3 sets of 12 reps</li>
+        <li>Tricep Rope Pull Down: 3 sets of 12-15 reps</li>
+        
+    </ul>
+​
+    
+​
+    <h3>Friday (Back/Biceps/Abs)</h3>
+    <ul>
+        <li>Barbell Bench Press: 3 sets of 8 reps</li>
+        <li>Barbell Squats 3 sets of 12 reps</li>
+        <li>Seated Cable Row: 3 sets of 10 reps</li>
+        <li>Seated Dumbbell Shoulder Press: 3 sets of 10 reps</li>
+        <li>Reverse Crunches: 3 sets of 12 reps</li>
+        <li>Knee Raises on Roman Chair: 4 sets of 10 reps</li>
+        <li>Oblique Crunches: 4 sets of 12 reps each side</li>
+    </ul>
+​
+   
+​
+    <h3>Sunday (Full Body)</h3>
+    <ul>
+        <li>Burpees: 3 sets of 30 seconds</li>
+        <li>Kettle Bell Swings: 3 sets of 15 reps</li>
+        <li>Dumbbell Squats: 3 sets of 12 reps</li>
+        <li>Flat Dumbbell Bench Press: 3 sets of 10 reps</li>
+        <li>Push Ups: 3 sets of 10 reps</li>
+        <li>Dumbbell Deadlift: 3 sets of 15 reps</li>
+        <li>Bent Over Barbell Row: 3 sets of 8 reps</li>
+        <li>Russian Twists: 3 sets of 45 seconds</li>
+        <li>Bicycle Kicks: 3 sets of 45 seconds</li>
+    </ul>
+​
         </div>
     )
 }
